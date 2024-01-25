09d649df028fb0f15d87825973e9e70c23d10c62
Michal Procházka <michal@prochazkaml.eu>
2024-01-25 21:06:11 +0100

    Added commandline options to the example program

--- example.c
+++ example.c
@@ -1,60 +1,121 @@
 #include <math.h>
+#include <argp.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdbool.h>
 #include <SDL/SDL.h>
 
 #include "modplay.h"
 
+// thx, https://gist.github.com/ictlyh/b9be0b020ae3d044dc75ef0caac01fbb
+
+/* Program documentation. */
+static const char doc[] = "MODPlay example program";
+
+/* A description of the arguments we accept. */
+static const char args_doc[] = "MODFILE";
+
+/* The options we understand. */
+static struct argp_option options[] = {
+	{ "disable-ch-info", 'd', 0, 0,  "disable live channel info during playback\n(enabled by default)" },
+	{ "start-order", 'o', "ORDER_ID", 0, "start playback from specified order\n(1 by default)" },
+	{ 0 }
+};
+
+static bool disable_ch_info = false;
+static int start_order = 1;
+static char *filename;
+
+/* Parse a single option. */
+static error_t parse_opt (int key, char *arg, struct argp_state *state) {
+	switch(key) {
+		case 'd':
+			disable_ch_info = true;
+			break;
+
+		case 'o':
+			start_order = atoi(arg);
+			break;
+
+		case ARGP_KEY_ARG:
+			if (state->arg_num >= 1)
+				// Too many arguments.
+				argp_usage (state);
+
+			filename = arg;
+			break;
+
+		case ARGP_KEY_END:
+			if (state->arg_num < 1)
+				/* Not enough arguments. */
+				argp_usage (state);
+
+			break;
+
+		default:
+			return ARGP_ERR_UNKNOWN;
+	}
+
+	return 0;
+}
+
+/* Our argp parser. */
+static struct argp argp = { options, parse_opt, args_doc, doc };
+
 #define SAMPLERATE 44100
 #define BARGRAPHSTEPS 48
 
-double bargraphvals[CHANNELS];
-double bargraphtargets[CHANNELS];
-double bargrapholdages[CHANNELS];
+static double bargraphvals[CHANNELS];
+static double bargraphtargets[CHANNELS];
+static double bargrapholdages[CHANNELS];
 
-int channels;
+static int channels;
 
 void SDL_Callback(void *data, uint8_t *stream, int len) {
 	short *buf = (short *) stream;
 
 	ModPlayerStatus_t *mp = RenderMOD(buf, len / (sizeof(short) * 2));
 
-	printf("\n\nID    | FREQ | VL | SM | BARGRAPH");
+	if(!disable_ch_info) {
+		printf("\n\nID    | FREQ | VL | SM | BARGRAPH");
 
-	for(int i = 0; i < channels; i++) {
-		printf("\n\r\e[2KCH %02d |% 5d |% 3d |% 3d | ",
-			i + 1,
-			mp->ch[i].samplegen.period ? (mp->paularate / mp->ch[i].samplegen.period) : 0,
-			mp->ch[i].samplegen.volume,
-			mp->ch[i].sample + 1);
+		for(int i = 0; i < channels; i++) {
+			printf("\n\r\e[2KCH %02d |% 5d |% 3d |% 3d | ",
+				i + 1,
+				mp->ch[i].samplegen.period ? (mp->paularate / mp->ch[i].samplegen.period) : 0,
+				mp->ch[i].samplegen.volume,
+				mp->ch[i].sample + 1);
 
-		bargraphtargets[i] = mp->ch[i].samplegen.volume;
+			bargraphtargets[i] = mp->ch[i].samplegen.volume;
 
-		if(mp->ch[i].samplegen.age < bargrapholdages[i])
-			bargraphtargets[i] *= 1.5;
+			if(mp->ch[i].samplegen.age < bargrapholdages[i])
+				bargraphtargets[i] *= 1.5;
 
-		bargrapholdages[i] = mp->ch[i].samplegen.age;
+			bargrapholdages[i] = mp->ch[i].samplegen.age;
 
-		if(mp->ch[i].period == 0) bargraphtargets[i] = 0.0;
-
-		if(bargraphvals[i] < bargraphtargets[i]) bargraphvals[i] = bargraphtargets[i];
-
-		for(int x = 0; x < BARGRAPHSTEPS; x++) {
-			if(bargraphvals[i] > x * 96 / BARGRAPHSTEPS)
-				putchar('=');
-			else
-				break;
-		}
-
-		if(bargraphvals[i] > bargraphtargets[i]) {
-			bargraphvals[i] -= 2;
+			if(mp->ch[i].period == 0) bargraphtargets[i] = 0.0;
 
 			if(bargraphvals[i] < bargraphtargets[i]) bargraphvals[i] = bargraphtargets[i];
+
+			for(int x = 0; x < BARGRAPHSTEPS; x++) {
+				if(bargraphvals[i] > x * 96 / BARGRAPHSTEPS)
+					putchar('=');
+				else
+					break;
+			}
+
+			if(bargraphvals[i] > bargraphtargets[i]) {
+				bargraphvals[i] -= 2;
+
+				if(bargraphvals[i] < bargraphtargets[i]) bargraphvals[i] = bargraphtargets[i];
+			}
 		}
+
+		printf("\e[%dA", 2 + channels);
 	}
 
-	printf("\e[%dA\r\e[2KRow %02d, order %02d/%02d (pattern %02d) @ speed %d", 2 + channels, mp->row, mp->order + 1, mp->orders, mp->ordertable[mp->order], mp->speed);
+	printf("\r\e[2KRow %02d, order %02d/%02d (pattern %02d) @ speed %d", mp->row, mp->order + 1, mp->orders, mp->ordertable[mp->order], mp->speed);
 
 	fflush(stdout);
 }
@@ -68,12 +129,9 @@ SDL_AudioSpec sdl_audio = {
 };
 
 int main(int argc, char *argv[]) {
-	if(argc != 2) {
-		printf("Usage: %s modfile\n", argv[0]);
-		exit(1);
-	}
+	argp_parse(&argp, argc, argv, 0, 0, NULL);
 
-	FILE *f = fopen(argv[1], "rb");
+	FILE *f = fopen(filename, "rb");
 
 	if(f == NULL) {
 		printf("Error loading file!\n");
@@ -97,7 +155,11 @@ int main(int argc, char *argv[]) {
 
 	channels = mp->channels;
 
-	printf("Status type size: %d\n", sizeof(ModPlayerStatus_t));
+	if(start_order > 1 && start_order <= mp->orders) {
+		JumpMOD(start_order - 1);
+	}
+
+//	printf("Status type size: %d\n", sizeof(ModPlayerStatus_t));
 
 	for(int i = 0; i < CHANNELS; i++) {
 		bargraphvals[i] = 0.0;
@@ -105,7 +167,7 @@ int main(int argc, char *argv[]) {
 		bargrapholdages[i] = INFINITY;
 	}
 
-	printf("Playing %s...\n\n", argv[1]);
+	printf("Playing %s...\n\n", filename);
 
 	SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO);
 	SDL_OpenAudio(&sdl_audio, 0);
