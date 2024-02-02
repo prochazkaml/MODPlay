51dd98bc251d159b50180995fd1f4d0bacc829e2
Michal Procházka <michal@prochazkaml.eu>
2024-02-02 10:07:56 +0100

    Now including various optimization levels in the table

--- README.md
+++ README.md
@@ -25,19 +25,35 @@ Compiled with the default number of channels:
 
 |Compiler|Arch|.text|.data|.bss
 |-|-|-|-|-|
-|clang 16.0.6|amd64|9599|0|3152|
-|clang 16.0.6|i386|7607|0|2632|
-|clang 16.0.6|aarch64|9876|0|3152|
-|clang 16.0.6|armv7m|6046|0|2632|
+|clang 16.0.6 (-O0)|amd64|9599|0|3152|
+|clang 16.0.6 (-O3)|amd64|6101|0|3152|
+|clang 16.0.6 (-Os)|amd64|4938|0|3152|
+|clang 16.0.6 (-O0)|i386|7607|0|2632|
+|clang 16.0.6 (-O3)|i386|5461|0|2632|
+|clang 16.0.6 (-Os)|i386|4386|0|2632|
+|clang 16.0.6 (-O0)|aarch64|9876|0|3152|
+|clang 16.0.6 (-O3)|aarch64|5504|0|3152|
+|clang 16.0.6 (-Os)|aarch64|4536|0|3152|
+|clang 16.0.6 (-O0)|armv7m|6046|0|2632|
+|clang 16.0.6 (-O3)|armv7m|4664|0|2632|
+|clang 16.0.6 (-Os)|armv7m|3164|0|2632|
 
 Compiled for 4 channels maximum:
 
 |Compiler|Arch|.text|.data|.bss
 |-|-|-|-|-|
-|clang 16.0.6|amd64|9599|0|912|
-|clang 16.0.6|i386|7607|0|616|
-|clang 16.0.6|aarch64|9876|0|912|
-|clang 16.0.6|armv7m|6008|0|616|
+|clang 16.0.6 (-O0)|amd64|9599|0|912|
+|clang 16.0.6 (-O3)|amd64|6101|0|912|
+|clang 16.0.6 (-Os)|amd64|4938|0|912|
+|clang 16.0.6 (-O0)|i386|7607|0|616|
+|clang 16.0.6 (-O3)|i386|5461|0|616|
+|clang 16.0.6 (-Os)|i386|4391|0|616|
+|clang 16.0.6 (-O0)|aarch64|9876|0|912|
+|clang 16.0.6 (-O3)|aarch64|5568|0|912|
+|clang 16.0.6 (-Os)|aarch64|4536|0|912|
+|clang 16.0.6 (-O0)|armv7m|6008|0|616|
+|clang 16.0.6 (-O3)|armv7m|4712|0|616|
+|clang 16.0.6 (-Os)|armv7m|3172|0|616|
 
 <!-- LIBRARY_SIZE_END -->
 
--- gen_sizes.sh
+++ gen_sizes.sh
@@ -11,21 +11,34 @@ clangversion=`clang --version | head -n 1 | cut -d" " -f3`
 echo "Using clang version $clangversion." 1>&2
 
 compilers=(
-	"clang -c modplay.c -o modplay.o -target amd64"
-	"clang -c modplay.c -o modplay.o -target i386"
-	"clang -c modplay.c -o modplay.o -target aarch64"
-	"clang -c modplay.c -o modplay.o -target armv7m"
+	"clang -c modplay.c -o modplay.o -target amd64 -Os"
+	"clang -c modplay.c -o modplay.o -target i386 -Os"
+	"clang -c modplay.c -o modplay.o -target aarch64 -Os"
+	"clang -c modplay.c -o modplay.o -target armv7m -Os"
 #	"clang -c modplay.c -o modplay.o -target riscv64"
 #	"clang -c modplay.c -o modplay.o -target riscv32"
 )
 
 compilersfriendly=(
-	"clang $clangversion|amd64"
-	"clang $clangversion|i386"
-	"clang $clangversion|aarch64"
-	"clang $clangversion|armv7m"
-#	"clang|riscv64"
-#	"clang|riscv32"
+	"clang $clangversion"
+	"clang $clangversion"
+	"clang $clangversion"
+	"clang $clangversion"
+#	"clang $clangversion"
+#	"clang $clangversion"
+)
+
+archsfriendly=(
+	"amd64"
+	"i386"
+	"aarch64"
+	"armv7m"
+)
+
+optimizations=(
+	"-O0"
+	"-O3"
+	"-Os"
 )
 
 channels=(
@@ -59,17 +72,18 @@ echo "$readme" | while read line; do
 			echo "Generating compiler table..." 1>&2
 
 			for i in "${!compilers[@]}"; do
-				rm -f modplay.o
-				compiler=${compilers[$i]}
-				compilerfriendly=${compilersfriendly[$i]}
-				echo "Running \"$compiler\"" 1>&2
-				$compiler ${channels[$j]}
-				size=`size modplay.o | grep modplay.o`
-				textsize=`echo "$size" | awk '{print $1}'`
-				datasize=`echo "$size" | awk '{print $2}'`
-				bsssize=`echo "$size" | awk '{print $3}'`
-				rm -f modplay.o
-				echo "|$compilerfriendly|$textsize|$datasize|$bsssize|"
+				for k in "${!optimizations[@]}"; do
+					rm -f modplay.o
+					compiler="${compilers[$i]} ${channels[$j]} ${optimizations[$k]}"
+					echo $compiler 1>&2
+					$compiler
+					size=`size modplay.o | grep modplay.o`
+					textsize=`echo "$size" | awk '{print $1}'`
+					datasize=`echo "$size" | awk '{print $2}'`
+					bsssize=`echo "$size" | awk '{print $3}'`
+					rm -f modplay.o
+					echo "|${compilersfriendly[$i]} (${optimizations[$k]})|${archsfriendly[$i]}|$textsize|$datasize|$bsssize|"
+				done
 			done
 
 			echo
